9cbe7b6bf12fe23c999148075aa329a4c9ddb0c8
morris.ma <rongjia1@asu.edu>
2022-08-14 13:32:57 -0700

    Update Collatz.Java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,15 +4,6 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-//    public static int nextNumber(int n) {
-//        if (n == 128) {
-//            return 1;
-//        } else if (n == 5) {
-//            return 3 * n + 1;
-//        } else {
-//            return n * 2;
-//        }
-//    }
     /** create a new function nextNumber to return next Number follow Collatz sequence*/
     public static int nextNumber(int n) {
         if (n % 2 == 0) {
@@ -23,7 +14,7 @@ public class Collatz {
     }
 
     public static void main(String[] args) {
-        int n = 27;
+        int n = 5;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
