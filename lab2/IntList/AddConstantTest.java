d04477d9f80c2e53a3b4af86579736942c9c5162
morris.ma <rongjia1@asu.edu>
2022-08-27 21:34:05 -0700

    modify addConstant to pass hidden test

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -25,4 +25,10 @@ public class AddConstantTest {
         IntListExercises.addConstant(lst, 10);
         assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
     }
+    @Test
+    public void testAddToSingleList() {
+        IntList lst = IntList.of(1);
+        IntListExercises.addConstant(lst, 10);
+        assertEquals("11", lst.toString());
+    }
 }
