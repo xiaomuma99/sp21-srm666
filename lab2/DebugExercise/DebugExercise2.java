73064cc6f28975f2b5f2222ab4d9e5fe468cb4be
morris.ma <rongjia1@asu.edu>
2022-08-27 19:56:45 -0700

    Complete lab2 debug homework

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -6,14 +6,23 @@ package DebugExercise;
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
-    public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+//    public static int max(int a, int b) {
+//        int w = (b - a) >> 31;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int z = ~(b - a) >> 31;
+//
+//        int max = b & w | a & z;
+//        return max;
+//    }
+    /**
+     * Write a new method max to return large the value of two numbers
+     */
+    public static int max(int a, int b){
+        if(a > b){
+            return a;
+        }
+        return b;
     }
 
 
@@ -54,11 +63,23 @@ public class DebugExercise2 {
     }
 
     /** Returns the sum of all elements in x. */
-    public static int arraySum(int[] x) {
+//    public static int arraySum(int[] x) {
+//        int i = 0;
+//        int sum = 0;
+//        while (i < x.length) {
+//            sum = sum + add(sum, x[i]);
+//            i = i + 1;
+//        }
+//        return sum;
+//    }
+    /**
+     * Write a new method arraySum to calculate the sum of all elements in x.
+     */
+    public static int arraySum(int[] x){
         int i = 0;
         int sum = 0;
-        while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+        while(i < x.length){
+            sum = sum + x[i];
             i = i + 1;
         }
         return sum;
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -12,9 +12,15 @@ public class SquarePrimesTest {
      */
     @Test
     public void testSquarePrimesSimple() {
-        IntList lst = IntList.of(14, 15, 16, 17, 18);
-        boolean changed = IntListExercises.squarePrimes(lst);
-        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+//        IntList lst = IntList.of(14, 15, 16, 17, 18);
+//        boolean changed = IntListExercises.squarePrimes(lst);
+//        IntList lst2 = IntList.of(5, 7, 8, 10, 12);
+//        boolean changed = IntListExercises.squarePrimes(lst2);
+        IntList lst3 = IntList.of(4, 6, 8, 10, 13);
+        boolean changed = IntListExercises.squarePrimes(lst3);
+
+//        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertEquals("4 -> 6 -> 8 -> 10 -> 169", lst3.toString());
         assertTrue(changed);
     }
 }
